c67a9d8de0d255e0d5a18dc71718162e1bfcdc19
hamza1120 <hamzabouhouchi02@gmail.com>
2023-08-03 13:26:47 +0100

    props

---
+++ fifa-player-cards/src/players.js
@@ -0,0 +1,22 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Paris Saint-Germain",
+    nationality: "Argentina",
+    jerseyNumber: 10,
+    age: 34,
+    imageUrl:
+      "https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg)/origin-imgresizer.eurosport.com/2023/07/26/3751256-76315048-2560-1440.jpg",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Manchester United",
+    nationality: "Portugal",
+    jerseyNumber: 7,
+    age: 36,
+    imageUrl:
+      "https://static01.nyt.com/images/2022/12/30/multimedia/30soccer-ronaldo-1-76fd/30soccer-ronaldo-1-76fd-mediumSquareAt3X.jpg",
+  },
+];
+
+export default players;
